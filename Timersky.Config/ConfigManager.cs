08869b010dd9d4f2a2c88e44ae9a99fab80025e7
Ivan Timersky <96748043+Time2138@users.noreply.github.com>
2024-12-02 02:19:36 +0300

    add summarys & debug

--- Timersky.Config/ConfigManager.cs
+++ Timersky.Config/ConfigManager.cs
@@ -1,6 +1,5 @@
 using Timersky.Log;
 using Tomlet;
-using Tomlet.Models;
 
 namespace Timersky.Config;
 
@@ -9,47 +8,52 @@ public sealed class ConfigManager
     private static readonly string ConfigFilePath = $"{AppDomain.CurrentDomain.BaseDirectory}Config.toml";
     private static readonly Logger Log = new();
     
-    private static TCfg GenConfig<TCfg>() where TCfg : IConfig
+    private static T GenConfig<T>() where T : IConfig
     {
         File.Create(ConfigFilePath).Close();
             
-        TCfg config = Activator.CreateInstance<TCfg>();
-            
-        using (FileStream file = new(ConfigFilePath, FileMode.Append))
-        {
-            using (StreamWriter writer = new(file))
-            {
-                writer.WriteLine(TomletMain.TomlStringFrom(config));
-            }
-        }
-            
+        T config = Activator.CreateInstance<T>();
+
+        using FileStream file = new(ConfigFilePath, FileMode.Append);
+        using StreamWriter writer = new(file);
+        writer.WriteLine(TomletMain.TomlStringFrom(config));
+
         return config;
     }
 
-    public static TCfg LoadConfig<TCfg>() where TCfg : IConfig
+    /// <summary>
+    /// Loads the configuration of the specified type from a TOML file.
+    /// If the file does not exist, a new configuration is generated and returned.
+    /// In case of a parsing error, the invalid file is deleted, and a new configuration is generated.
+    /// </summary>
+    /// <typeparam name="T">The type of the configuration to load. Must implement the <see cref="IConfig"/> interface.</typeparam>
+    /// <returns>An instance of the configuration object of type <typeparamref name="T"/>.</returns>
+    /// <exception cref="Exception">Logs and handles exceptions that occur during configuration loading.</exception>
+    public static T LoadConfig<T>() where T : IConfig
     {
-        TCfg config;
-        
+        T config;
+    
         if (!File.Exists(ConfigFilePath))
         {
-            config = GenConfig<TCfg>();
+            config = GenConfig<T>();
         }
         else
         {
             try
             {
-                config = TomletMain.To<TCfg>(TomlParser.ParseFile(ConfigFilePath));
+                config = TomletMain.To<T>(TomlParser.ParseFile(ConfigFilePath));
             }
             catch (Exception e)
             {
                 Log.Error(e.Message);
-                
+            
                 File.Delete(ConfigFilePath);
-                
-                config = GenConfig<TCfg>();
+            
+                config = GenConfig<T>();
             }
         }
         
+        Logger.DebugIsAllowed = config.Debug;
         return config;
     }
 }
--- Timersky.Config/IConfig.cs
+++ Timersky.Config/IConfig.cs
@@ -1,3 +1,16 @@
+using Tomlet.Attributes;
+
 namespace Timersky.Config;
 
-public interface IConfig { }
+/// <summary>
+/// Represents a configuration structure with a debug property, 
+/// allowing it to be mapped from a TOML file.
+/// </summary>
+public interface IConfig
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether debugging is enabled.
+    /// </summary>
+    [TomlProperty("debug")]
+    public bool Debug { get; set; }
+}
