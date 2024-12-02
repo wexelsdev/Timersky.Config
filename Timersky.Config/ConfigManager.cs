9dc6caf35bf46cc120c56f65111480a117c78358
Ivan Timersky <96748043+Time2138@users.noreply.github.com>
2024-12-02 13:51:11 +0300

    upd

--- Timersky.Config/ConfigManager.cs
+++ Timersky.Config/ConfigManager.cs
@@ -5,16 +5,15 @@ namespace Timersky.Config;
 
 public sealed class ConfigManager
 {
-    private static readonly string ConfigFilePath = $"{AppDomain.CurrentDomain.BaseDirectory}Config.toml";
     private static readonly Logger Log = new();
     
-    private static T GenConfig<T>() where T : IConfig
+    private static T GenConfig<T>(string path) where T : IConfig
     {
-        File.Create(ConfigFilePath).Close();
+        File.Create(path).Close();
             
         T config = Activator.CreateInstance<T>();
 
-        using FileStream file = new(ConfigFilePath, FileMode.Append);
+        using FileStream file = new(path, FileMode.Append);
         using StreamWriter writer = new(file);
         writer.WriteLine(TomletMain.TomlStringFrom(config));
 
@@ -26,30 +25,39 @@ public sealed class ConfigManager
     /// If the file does not exist, a new configuration is generated and returned.
     /// In case of a parsing error, the invalid file is deleted, and a new configuration is generated.
     /// </summary>
+    /// <param name="path">
+    /// The file path where config will be stored. 
+    /// If not specified, defaults to a "Config.toml" file in the application's base directory.
+    /// </param>
     /// <typeparam name="T">The type of the configuration to load. Must implement the <see cref="IConfig"/> interface.</typeparam>
     /// <returns>An instance of the configuration object of type <typeparamref name="T"/>.</returns>
     /// <exception cref="Exception">Logs and handles exceptions that occur during configuration loading.</exception>
-    public static T LoadConfig<T>() where T : IConfig
+    public static T LoadConfig<T>(string path = "") where T : IConfig
     {
+        if (string.IsNullOrEmpty(path))
+        {
+            path = $"{AppDomain.CurrentDomain.BaseDirectory}Config.toml";
+        }
+        
         T config;
     
-        if (!File.Exists(ConfigFilePath))
+        if (!File.Exists(path))
         {
-            config = GenConfig<T>();
+            config = GenConfig<T>(path);
         }
         else
         {
             try
             {
-                config = TomletMain.To<T>(TomlParser.ParseFile(ConfigFilePath));
+                config = TomletMain.To<T>(TomlParser.ParseFile(path));
             }
             catch (Exception e)
             {
                 Log.Error(e.Message);
             
-                File.Delete(ConfigFilePath);
+                File.Delete(path);
             
-                config = GenConfig<T>();
+                config = GenConfig<T>(path);
             }
         }
         
